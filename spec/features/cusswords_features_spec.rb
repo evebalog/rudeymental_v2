b60c33ff0eb13a8d46e105e02b704924831c95ca
Eva Balog <evebalog@netscape.net>
2017-04-16 15:43:35 +0100

    Added third filter option, feature tests passing

--- spec/features/cusswords_features_spec.rb
+++ spec/features/cusswords_features_spec.rb
@@ -7,11 +7,12 @@ feature "Cusswords page" do
       expect(page).to have_content "How rudey are you feeling today?"
     end
 
-    context "displaying cusswords" do
+    context "displaying cusswords with rating filters" do
 
       before do
         Cussword.create(word: "Duckmaster", rating: 1)
         Cussword.create(word: "Shitmaster", rating: 2)
+        Cussword.create(word: "Fwordmaster", rating: 3)
       end
       # test below was only used before introducing filters
       # scenario "user can see cusswords" do
@@ -30,6 +31,14 @@ feature "Cusswords page" do
         visit "/cusswords"
         click_button "Mildly Irritated"
         expect(page).to have_content "Shitmaster"
+        expect(page).to have_no_content "Duckmaster"
+      end
+
+      scenario "user can click on 'Positively Livid' button and see cusswords with 3 rating only" do
+        visit "/cusswords"
+        click_button "Positively Livid"
+        expect(page).to have_content "Fwordmaster"
+        expect(page).to have_no_content "Shitmaster"
         expect(page).to have_no_content "Duckmaster"
       end
     end
